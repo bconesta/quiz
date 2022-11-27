18eed6f620c3497ff2b281dae2fa3870dc90ded4
Bruno Conesta <bconesta@etrr.edu.ar>
2022-11-27 16:47:54 -0300

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.scss';
+
+const styles_none = {
+  'display' : 'none'
+};
+
+const styles_block = {
+  'display' : 'inline-flex'
+};
+
+let styles_l;
+let styles_r;
+
+const signos = [
+  '<',
+  '>'
+]
+
+const texto = [
+  'Hola Juanjo,',
+  'quería contarte algo importante.',
+  'El día en el que te pedí que me expliques',
+  'en realidad,',
+  'era para esto',
+  'pero',
+  'me frustro mucho la actitud',
+  'y por eso me lo tome muy mal',
+  'y no tuve ganas de hablar',
+  'me gustaría poder charlarlo',
+  'y quería también',
+  'saber si',
+  'querrías entregarme la medalla'
+]
 
 function App() {
+  const [selector, setSelector] = useState(0);
+
+  if(selector==0){
+    styles_l = styles_none;
+  }
+  else{
+    styles_l = styles_block;
+  }
+  if(selector<texto.length-1){
+    styles_r = styles_block;
+  }
+  else{
+    styles_r = styles_none;
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1>{texto[selector]}</h1>
+        <a style={styles_l} onClick={()=>setSelector(selector-1)} >{signos[0]}</a>
+        <a style={styles_r} onClick={()=>setSelector(selector+1)} >{signos[1]}</a>
+      </div>
     </div>
   );
 }
